eb5096a1e501eb6c83553df499d0cdd9baa8fd64
tcavalini <cavalini@gmail.com>
2024-08-19 10:52:32 -0300

    hello

---
+++ java-anatomina-classes/src/MinhaClasse.java
@@ -0,0 +1,10 @@
+public class MinhaClasse {
+    public static void main(String [] args){
+        int ano = 2021.;
+
+        ano = 2022;
+
+        final String BR = "Brasil";
+        BR = "Beazil";
+    }
+}
